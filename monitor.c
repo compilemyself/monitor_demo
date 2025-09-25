bbd56b2ffc4c898b4d658f7d5b7947c7c0e2f106
willian <willowbrunn@gmail.com>
2025-09-25 15:56:48 -0300

    atualização das bibliotecas

--- monitor.c
+++ monitor.c
@@ -1,9 +1,11 @@
 // Estas são as bibliotecas utilizadas para a funcionalidade do nosso monitor de rede:
 
 #include <stdio.h>       // Biblioteca de entrada e saída (ex.: printf), amplamente utilizada em diferentes aplicações
-#include <arpa/inet.h>   // Funções de conversão de endereços (inet_ntoa, inet_pton, etc.)
-#include <sys/socket.h>  // Define estruturas e funções para sockets (pontos de comunicação usados em rede)
-#include <ifaddrs.h>     // Permite obter informações de todas as interfaces de rede do sistema
+#include <stdlib.h>      // Biblioteca com funções utilitárias como alocação de memória e execução de comandos externos
+#include <string.h>      // Biblioteca para manipulação de strings, oferecendo funções de busca, cópia e formatação
+#include <sys/socket.h>  // Define estruturas e funções para criação e uso de sockets (pontos de comunicação em rede)
+#include <arpa/inet.h>   // Biblioteca usada para conversão de endereços de rede entre formatos humano e binário
+#include <ifaddrs.h>     // Permite coletar informações sobre todas as interfaces de rede disponíveis no sistema
 
 int ipv6_printed = 0; // Marcador futuramente usado para separar os endereços IPv4 e IPv6 com uma linha em branco
 
